df66312a8e415afc591875f791f6f40b4dd193bb
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-01 22:06:58 +0000

    FEAT: implement task 0 of the project

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Create a Cache class. In the __init__ method, store an instance
+of the Redis client as a private variable named _redis
+(using redis.Redis()) and flush the instance using flushdb.
+"""
+import redis
+from uuid import uuid4
+from functools import wraps
+from typing import Union, Optional, Callable
+
+
+class Cache:
+    """Creating a Cache Class"""
+
+    def __init__(self):
+        """Dunder function to store the instance of Redis Client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Function that generates a random key"""
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
+        return random_key
+
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        """Function that convert data back to required format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Function automatically parameterize cache.get"""
+        value - self._redis.get(key)
+        return value.decode('utf-8')
+
+    def get_int(self, key: str) -> int:
+        """function aht parameterize cache.get to integer"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
