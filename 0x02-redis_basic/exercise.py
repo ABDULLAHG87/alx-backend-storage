ee4dd6299afc0fe852085b7c944a74b7d65b383c
Abdullahi <hakeemabdullah87@gmail.com>
2024-05-01 23:08:16 +0000

    UPDATE: update the exercise file by adding the call-hhistory method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,20 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Method for storing the history of inputs and outputs"""
+    @wrap(method)
+    def wrapper(self, *args, **kwargs):
+        """method that wraps call history decorated function"""
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":ouptuts", output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     """Creating a Cache Class"""
 
